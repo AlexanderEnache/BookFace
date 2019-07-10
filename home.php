37de8dab74d06210d689f8392d3cbf2c258560de
Alex Enache <ragnarxon@gmail.com>
2019-07-10 17:08:43 -0400

    First push

---
+++ home.php
@@ -0,0 +1,14 @@
+<html>
+
+	<head>
+		<title>AssFace</title>
+		<link rel="stylesheet" type="text/css" href="style.css">
+	</head>
+	
+	<body>
+	<?php include 'navbar.php'; ?>
+		<h1>Home</h1>
+	</body>
+
+</html>
+
---
+++ navbar.php
@@ -0,0 +1,4 @@
+<div class = 'navbar'>
+	<h1 class='logo'>AssFace</h1>
+</div>
+
