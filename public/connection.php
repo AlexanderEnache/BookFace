020038562699558b75af6d4f5f8456c5b4120b6f
Alex Enache <ragnarxon@gmail.com>
2019-09-01 10:47:31 -0400

    testing heroku migration

--- public/connection.php
+++ public/connection.php
@@ -1,5 +1,11 @@
 <?php 
-	$connection = mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'bde82497d2b3f6', 'a2ada8ae', 'heroku_7a120ebf6cf312b');
+	//$connection = mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'bde82497d2b3f6', 'a2ada8ae', 'heroku_7a120ebf6cf312b');
+	
+// Username:	b43b77d43a86c7
+// Password:	bc504af6
+// Database:	heroku_f56b26996b464a9
+
+	$connection = mysqli_connect('us-cdbr-iron-east-02.cleardb.net', 'b43b77d43a86c7', 'bc504af6', 'heroku_f56b26996b464a9');
 	
 	//$connection = mysqli_connect('localhost', 'root', '', 'user');
 ?>
